41fec24ef40222c475a42cd78353b813ba90cb24
Calvin Roberts <you@example.com>
2023-05-30 20:08:23 -0600

    started adding JS to app.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,11 @@
-var apiKey = "53736836ebc94d892bdc0b54c089eeab";
-var fiveDayForcast;
-var queryURL = ""
+function runAPI(){
+    var apiKey = "53736836ebc94d892bdc0b54c089eeab";
+    var city = document.getElementById("searchBox").value;
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey;
 
-fetch(queryURL);
+    fetch(queryURL)
+        .then(function(response){
+            return response.json();
+        }) 
+        console.log(response)
+}
