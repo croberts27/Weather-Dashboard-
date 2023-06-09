bc37cd03a35e77d956c0b532cadf77f01714c818
Calvin Roberts <you@example.com>
2023-06-09 15:51:07 -0600

    updated js and HTML

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,31 @@
-function runAPI(){
+function runAPI() {
     var apiKey = "53736836ebc94d892bdc0b54c089eeab";
     var city = document.getElementById("searchBox").value;
-    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey;
 
     fetch(queryURL)
-        .then(function(response){
+        .then(function (response) {
             return response.json();
-        }) 
-        console.log(response)
+        })
+        .then(function (data) {
+            console.log(data); // Log the weather data to the console
+
+            var currentWeatherElement = document.getElementById("currentWeather");
+            currentWeatherElement.innerHTML = "Temperature: " + data[0].temperature + "°C";
+
+            var day1Element = document.getElementById("day1");
+            day1Element.textContent = "Monday";
+
+            var day2Element = document.getElementById("day2");
+            day2Element.textContent = "Tuesday";
+
+            var day3Element = document.getElementById("day3");
+            day3Element.textContent = "Wednesday";
+
+            var day4Element = document.getElementById("day4");
+            day4Element.textContent = "Thursday";
+
+            var day5Element = document.getElementById("day5");
+            day5Element.textContent = "Friday";
+        });
 }
